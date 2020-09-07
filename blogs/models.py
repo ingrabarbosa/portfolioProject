0c6c7c3f269887c607884ab250529a1dccb1e6fd
ingrambarbosa <ingra_mba@hotmail.com>
2020-09-07 16:30:30 -0300

    Blog functionality and polish

--- blogs/models.py
+++ blogs/models.py
@@ -7,3 +7,12 @@ class Blog(models.Model):
 	body = models.TextField()
 	image = models.ImageField(upload_to='images/') #whenever someone uploads an image it will be saved in 'images/'
 
+	def __str__(self):
+		return self.title
+
+	def summary(self):
+		return self.body[:100]
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%d of %B of %Y')
+
--- jobs/models.py
+++ jobs/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Job(models.Model):
 	image = models.ImageField(upload_to='images/') #whenever someone uploads an image it will be saved in 'images/'
 	summary = models.CharField(max_length=200)
+
+	
